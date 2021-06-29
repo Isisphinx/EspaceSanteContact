9100263aab68795a4804fcba893afe1a8e7d036d
Thomas <mrthomas.couteau@gmail.com>
2021-06-29 15:02:34 +0200

    maj contactListe

--- src/components/contactListe.js
+++ src/components/contactListe.js
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React from 'react'
 //Pour les composants MATERIAL-UI
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
@@ -11,44 +11,42 @@ import DraftsIcon from '@material-ui/icons/Drafts';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      width: '100%',
-      maxWidth: 360,
-      backgroundColor: theme.palette.background.paper,
+        width: '100%',
+        maxWidth: 360,
+        backgroundColor: theme.palette.background.paper,
     },
-  }));
-  
-  function ListItemLink(props) {
-    return <ListItem button component="a" {...props} />;
-  }
-  
-  export default function SimpleList() {
+}));
+
+
+export default function SimpleList() {
     const classes = useStyles();
-  
+
     return (
-      <div className={classes.root}>
-        <List component="nav" aria-label="main mailbox folders">
-          <ListItem button>
-            <ListItemIcon>
-              <InboxIcon />
-            </ListItemIcon>
-            <ListItemText primary="Inbox" />
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <DraftsIcon />
-            </ListItemIcon>
-            <ListItemText primary="Drafts" />
-          </ListItem>
-        </List>
-        <Divider />
-        <List component="nav" aria-label="secondary mailbox folders">
-          <ListItem button>
-            <ListItemText primary="Trash" />
-          </ListItem>
-          <ListItemLink href="#simple-list">
-            <ListItemText primary="Spam" />
-          </ListItemLink>
-        </List>
-      </div>
+        <div className={classes.root}>
+            <List component="nav" aria-label="main mailbox folders">
+                <ListItem button>
+                    <ListItemIcon>
+                        <InboxIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Inbox" />
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <DraftsIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Drafts" />
+                </ListItem>
+            </List>
+            <Divider />
+            <List component="nav" aria-label="secondary mailbox folders">
+                <ListItem button>
+                    <ListItemText primary="Trash" />
+                </ListItem>
+                <ListItemLink href="#simple-list">
+                    <ListItemText primary="Spam" />
+                </ListItemLink>
+            </List>
+        </div>
     );
-  }
+}
+export default useStyles
