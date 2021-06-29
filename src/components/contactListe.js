debcf0ac7277f0977dac8842e05d623fc19d7795
Thomas <mrthomas.couteau@gmail.com>
2021-06-29 14:27:57 +0200

    test d'un composant de liste

--- src/components/contactListe.js
+++ src/components/contactListe.js
@@ -1,10 +1,26 @@
 //Auteur : COUTEAU Thomas
 //le 29/06/21
+//Liste de contacts
 import * as React from "react"
+//Pour les composants MATERIAL-UI
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
 
-const contactListe = () => {
-    return (
-        <p>test</p>
-    )
-}
+const contactListe = makeStyles((theme) => ({
+    root: {
+      width: '100%',
+      maxWidth: 360,
+      backgroundColor: theme.palette.background.paper,
+    },
+  }));
+  
+  function ListItemLink(props) {
+    return <ListItem button component="a" {...props} />;
+  }
 export default contactListe
