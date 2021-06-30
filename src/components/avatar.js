b3486e79653dcc61f5fdaf6afbfc113c0a4f338d
Thomas <mrthomas.couteau@gmail.com>
2021-06-30 16:41:43 +0200

    .

--- src/components/avatar.js
+++ src/components/avatar.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
@@ -12,7 +11,7 @@ import faceSmall from '../images/face-small.jpg';
 const useStyles = makeStyles((theme) => ({
     root: {
         width: '100%',
-        maxWidth: '36ch',
+        maxWidth: '40ch',
         backgroundColor: theme.palette.background.paper,
     },
     inline: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@ import ContactList from '../components/contactList';
 const IndexPage = () => (
 
   <main>
-    <link rel="icon" type="image/png" sizes="16x16" href="https://www.1formatik.com/images/favicon/favicon-16x16.png"></link>
     <title>EspaceSanteContact</title>
     <div>
       <p>iadaizobzbdouazbdabdabdaozbdaizdbipazbd</p>
