37e24c94490ede114de4c6aa0a0eb79c5b9a847d
Thomas <mrthomas.couteau@gmail.com>
2021-06-30 15:45:24 +0200

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-import React from "react"
-import Layout from '../components/layout'
-import SimpleList from '../components/contactListe'
-import AlignItemsList from "../components/avatar"
-=======
-import React from 'react'
-import ContactList from '../components/contactList'
->>>>>>> a7fdce5494c91badd1b4928ba416ad020f19d813
+import React from "react";
+import SimpleList from '../components/contactListe';
+import AlignItemsList from "../components/avatar";
+import ContactList from '../components/contactList';
 
 const IndexPage = () => (
 
@@ -15,12 +10,9 @@ const IndexPage = () => (
       <div>
         <p>iadaizobzbdouazbdabdabdaozbdaizdbipazbd</p>
       </div>
-<<<<<<< HEAD
       <AlignItemsList/>
       <SimpleList/>
-=======
       <ContactList />
->>>>>>> a7fdce5494c91badd1b4928ba416ad020f19d813
     </main>
 
 )
