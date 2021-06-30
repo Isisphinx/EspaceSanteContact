7c84097f9c60794a2b120250d6c10b75827ff9e0
Thomas <mrthomas.couteau@gmail.com>
2021-06-30 17:27:40 +0200

    changement du favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
-
 import ContactList from '../components/contactList'
 //import FavIcon from '../components/favIcon'
+import favicon from '../images/icon.png'
+import Helmet from 'react-helmet'
 
 const IndexPage = () => (
   <main>
+    <Helmet>
+      <link rel="icon" href={favicon} />
+    </Helmet>
     <title>EspaceSanteContact</title>
     <div>
       <p>iadaizobzbdouazbdabdabdaozbdaizdbipazbd</p>
