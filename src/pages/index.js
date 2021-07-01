eae930cc9be51427ff9945be298bb375aae6f5d2
Thomas <mrthomas.couteau@gmail.com>
2021-07-01 16:39:47 +0200

    deco h1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ContactList from '../components/contactList'
-//import FavIcon from '../components/favIcon'
 import favicon from '../images/icon.png'
 import Helmet from 'react-helmet'
+import h1 from '../components/style.css'
+
 
 const IndexPage = () => (
   <main>
@@ -11,10 +12,8 @@ const IndexPage = () => (
       <title>EspaceSanteContact</title>
     </Helmet>
     <div>
-      <h1>EspaceSanteContact</h1>
-      <p>iadaizobzbdouazbdabdabdaozbdaizdbipazbd</p>
+      <h1 className={h1}>EspaceSanteContact</h1>
     </div>
-    
     <ContactList />
   </main>
 )
