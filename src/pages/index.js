1eda20ff4993292ac0f4ad020126f65e5239e961
Thomas <mrthomas.couteau@gmail.com>
2021-07-01 16:58:38 +0200

    mise en forme de la balise h2

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,15 @@ import React from 'react'
 import ContactList from '../components/contactList'
 import favicon from '../images/icon.png'
 import Helmet from 'react-helmet'
-import h1 from '../components/style.css'
-
+import h2 from '../components/style.css'
 
 const IndexPage = () => (
   <main>
     <Helmet>
-      <link rel="icon" href={favicon} />
+      <link rel='icon' href={favicon} />
       <title>EspaceSanteContact</title>
     </Helmet>
-    <div>
-      <h1 className={h1}>EspaceSanteContact</h1>
-    </div>
+    <h2 className={h2}>EspaceSanteContact</h2>
     <ContactList />
   </main>
 )
