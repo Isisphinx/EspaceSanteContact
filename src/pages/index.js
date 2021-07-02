d9f7f0584770545c9dbd1a04971a51d132c7cc7b
Tetraib <thibaut.constant@isisphinx.com>
2021-07-02 14:50:56 +0200

    use a favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,18 @@
-import React from "react";
-import AlignItemsList from "../components/avatar";
-import ContactList from '../components/contactList';
+import React from 'react'
+import Helmet from 'react-helmet'
+import AlignItemsList from '../components/avatar'
+import ContactList from '../components/contactList'
+import favicon from '../images/icon.png'
 
 const IndexPage = () => (
-
   <main>
     <title>EspaceSanteContact</title>
-    <div>
-      <p>iadaizobzbdouazbdabdabdaozbdaizdbipazbd</p>
-    </div>
+    <Helmet>
+      <link rel='icon' href={favicon} />
+    </Helmet>
     <AlignItemsList />
     <ContactList />
   </main>
-
 )
 
 export default IndexPage
