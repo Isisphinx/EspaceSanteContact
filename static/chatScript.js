b404c0c67cd7bda8f6101cc7bdb486e1e7529bc3
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2022-03-28 10:24:13 +0200

    Update chatScript.js

--- static/chatScript.js
+++ static/chatScript.js
@@ -1,10 +1,10 @@
 ;(function (d, t) {
-  var BASE_URL = 'https://chatwoot.espacesante.fr'
+  var BASE_URL = 'https://chat.espacesante.fr'
   var g = d.createElement(t),
     s = d.getElementsByTagName(t)[0]
   g.src = BASE_URL + '/packs/js/sdk.js'
   s.parentNode.insertBefore(g, s)
   g.onload = function () {
-    window.chatwootSDK.run({ websiteToken: 'PEGrTmGXvxo7yVuqcPNK2ttp', baseUrl: BASE_URL })
+    window.chatwootSDK.run({ websiteToken: 'ZgYrodSLd6UreR8dp1yNWw6r', baseUrl: BASE_URL })
   }
 })(document, 'script')
